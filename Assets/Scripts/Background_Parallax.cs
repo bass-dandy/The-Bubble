1d7a272257f273f8cdf7b24a513bf0682cc60eb0
naughtyfiddle <ctdinh93@gmail.com>
2014-12-24 00:40:44 -0600

    Add parallax transition from trench to 2nd area.

--- Assets/Scripts/Background_Parallax.cs
+++ Assets/Scripts/Background_Parallax.cs
@@ -8,10 +8,14 @@ public class Background_Parallax : MonoBehaviour {
 	public Transform middle;
 	public Transform back;
 	
+	public float frontOfs;
+	public float middleOfs;
+	public float backOfs;
+	
 
 	void Update () {
-		front.position  = new Vector3(cameraPos.position.x / 4, cameraPos.position.y / 4,front.position.z);
-		middle.position = new Vector3(cameraPos.position.x / 2, cameraPos.position.y / 2, middle.position.z);
-		back.position   = new Vector3(cameraPos.position.x, cameraPos.position.y, back.position.z);
+		front.position  = new Vector3(0.0f, cameraPos.position.y / 4 + frontOfs,front.position.z);
+		middle.position = new Vector3(0.0f, cameraPos.position.y / 2 + middleOfs, middle.position.z);
+		back.position   = new Vector3(0.0f, cameraPos.position.y + backOfs, back.position.z);
 	}
 }
