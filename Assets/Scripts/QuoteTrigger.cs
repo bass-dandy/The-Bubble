d5c9359dad6619aca034c577f807bebd7854164a
naughtyfiddle <ctdinh93@gmail.com>
2015-01-08 18:44:15 -0600

    Add triggers for quote spawning, fade in/out script for quotes

---
+++ Assets/Scripts/QuoteTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuoteTrigger : MonoBehaviour {
+
+	public GameObject panel;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player") {
+			Instantiate(panel);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/TimedDestroy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimedDestroy : MonoBehaviour {
+
+	public float duration;
+	public float fadeDuration;
+	private CanvasGroup cg;
+
+	void Start() {
+		cg = GetComponent<CanvasGroup>();
+		StartCoroutine(FadeIn());
+	}
+
+	IEnumerator FadeIn() {
+		while(cg.alpha < 1.0f) {
+			cg.alpha += fadeDuration * Time.deltaTime;
+			yield return null;
+		}
+		cg.alpha = 1.0f;
+		StartCoroutine(Countdown());
+	}
+
+	IEnumerator Countdown() {
+		while(duration > 0) {
+			duration -= Time.deltaTime;
+			yield return null;
+		}
+		StartCoroutine(FadeOut());
+	}
+	
+	IEnumerator FadeOut() {
+		while(cg.alpha > 0) {
+			cg.alpha -= fadeDuration * Time.deltaTime;
+			yield return null;
+		}
+		Destroy(gameObject);
+	}
+}
