cfc1c028b693232652a29f41ef1707b6e5597687
Christian Dinh <ctdinh93@gmail.com>
2015-02-13 03:34:50 -0600

    Add dynamic music, need some better loops

---
+++ Assets/Scripts/MusicTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicTrigger : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player") {
+			SendMessageUpwards("Advance");
+			Destroy(gameObject)	;
+		}
+	}
+}
